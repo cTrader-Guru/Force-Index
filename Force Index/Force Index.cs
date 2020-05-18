1e9e0549cf5ef02f71b8116c39e222c1e1423f0d
Leonardo <leonardo.ciaccio@gmail.com>
2020-05-18 11:56:16 +0200

    Aggiungere i file di progetto.

---
+++ Force Index/Force Index.cs
@@ -0,0 +1,101 @@
+﻿/*  CTRADER GURU --> Indicator Template 1.0.6
+
+    Homepage    : https://ctrader.guru/
+    Telegram    : https://t.me/ctraderguru
+    Twitter     : https://twitter.com/cTraderGURU/
+    Facebook    : https://www.facebook.com/ctrader.guru/
+    YouTube     : https://www.youtube.com/channel/UCKkgbw09Fifj65W5t5lHeCQ
+    GitHub      : https://github.com/ctrader-guru
+
+*/
+
+using cAlgo.API;
+using cAlgo.API.Indicators;
+using cAlgo.Indicators;
+
+namespace cAlgo
+{
+
+    [Indicator(IsOverlay = false, TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    [Levels(0)]
+    public class ForceIndex : Indicator
+    {
+
+        #region Enums
+
+        // --> Eventuali enumeratori li mettiamo qui
+
+        #endregion
+
+        #region Identity
+
+        /// <summary>
+        /// Nome del prodotto, identificativo, da modificare con il nome della propria creazione
+        /// </summary>
+        public const string NAME = "Force Index";
+
+        /// <summary>
+        /// La versione del prodotto, progressivo, utilie per controllare gli aggiornamenti se viene reso disponibile sul sito ctrader.guru
+        /// </summary>
+        public const string VERSION = "1.0.3";
+
+        #endregion
+
+        #region Params
+
+        /// <summary>
+        /// Identità del prodotto nel contesto di ctrader.guru
+        /// </summary>
+        [Parameter(NAME + " " + VERSION, Group = "Identity", DefaultValue = "https://ctrader.guru/product/force-index/")]
+        public string ProductInfo { get; set; }
+
+        [Parameter("Period", Group = "Params", DefaultValue = 14)]
+        public int Period { get; set; }
+
+        /// <summary>
+        /// L'output primario, può essere modificato a seconda delle esigenze
+        /// </summary>
+        [Output("Main", LineColor = "DodgerBlue")]
+        public IndicatorDataSeries Result { get; set; }
+
+        #endregion
+
+        #region Property
+
+        // --> Qui inseriremo variabili e costanti del progetto
+
+        #endregion
+
+        #region Indicator Events
+
+        /// <summary>
+        /// Viene generato all'avvio dell'indicatore, si inizializza l'indicatore
+        /// </summary>
+        protected override void Initialize()
+        {
+
+            // --> Stampo nei log la versione corrente
+            Print("{0} : {1}", NAME, VERSION);
+
+        }
+
+        /// <summary>
+        /// Generato ad ogni tick, vengono effettuati i calcoli dell'indicatore
+        /// </summary>
+        /// <param name="index">L'indice della candela in elaborazione</param>
+        public override void Calculate(int index)
+        {
+            
+            Result[index] = Bars.TickVolumes[index] * ( Bars.ClosePrices[ index ] - Bars.ClosePrices[ index - 1 ] );
+            
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Force Index
+Il massimo esponente tra gli indicatori per calibrare la volatilità
+
+[Pagina ufficiale](https://ctrader.guru/product/force-index/)
